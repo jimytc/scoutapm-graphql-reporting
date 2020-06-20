21045bf596837bfefee24dfd300cd0a829e7bbac
jimytc <solofat@gmail.com>
2020-06-20 15:45:24 +0800

    feat: enable scout reporting with three container serivces

--- app/graphql/scoutapm_graphql_reporting_schema.rb
+++ app/graphql/scoutapm_graphql_reporting_schema.rb
@@ -8,6 +8,19 @@ class ScoutapmGraphqlReportingSchema < GraphQL::Schema
   use GraphQL::Execution::Interpreter
   use GraphQL::Analysis::AST
 
+  if ENV['ENABLE_GRAPHQL_SCOUT']
+    Rails.logger.info('[Scout] Use GraphQL::Tracing::ScoutTracing')
+    if ENV['ENABLE_GRAPHQL_RENAME']
+      Rails.logger.info('[Scout][GraphQL::Tracing::ScoutTracing] set_transaction_name = true')
+      use GraphQL::Tracing::ScoutTracing, set_transaction_name: true
+    else
+      Rails.logger.info('[Scout][GraphQL::Tracing::ScoutTracing] set_transaction_name = nil')
+      use GraphQL::Tracing::ScoutTracing
+    end
+  else
+    Rails.logger.info('[Scout] Default Rails integration')
+  end
+
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
 end
